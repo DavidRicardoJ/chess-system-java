14df34f6cc488624ba721aa26b64fa6f4bead3e6
DavidRicardoJ <david_ricardo@live.com>
2019-01-28 22:24:31 -0200

    Switching player each turn.

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -7,14 +7,27 @@ import chess.chess.pieces.King;
 import chess.chess.pieces.Rook;
 
 public class ChessMatch {
+    private int turn;
+    private Color currentPlayer;
     private Board board;
 
     public ChessMatch() {
+
         board = new Board(8, 8);
+        turn=1;
+        currentPlayer = Color.WHITE;
         initialSetup();
     }
 
-    public ChessPiece[][] getPieces() {
+    public int getTurn() {
+        return turn;
+    }
+
+    public Color getCurrentPlayer() {
+        return currentPlayer;
+    }
+
+    public  ChessPiece[][] getPieces() {
         ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
         for (int i = 0; i < board.getRows(); i++) {
             for (int j = 0; j < board.getColumns(); j++) {
@@ -34,6 +47,7 @@ public class ChessMatch {
         validateSourcePosition(source);
         validateTargetPosition(source,target);
         Piece capturedPiece = makeMove(source,target);
+        nextTurn();
         return (ChessPiece)capturedPiece;
     }
     private Piece makeMove (Position source, Position target){
@@ -47,12 +61,19 @@ public class ChessMatch {
         if (! board.thereIsAPiece(position)){
             throw new ChessException("There is no piece on source position.");
         }
+        if (currentPlayer != ((ChessPiece)board.piece(position)).getColor()){
+            throw new ChessException("The chosen piece is not yours.");
+        }
         if (!board.piece(position).isThereAnyPossibleMove()){
             throw new ChessException("There is no possible moves for the chosen piece.");
         }
     }
     private void placeNewPiece (char column, int row, ChessPiece piece){
         board.placePiece(piece,new ChessPosition(column,row).toPosition());
+    }
+    private void nextTurn(){
+        turn ++;
+        currentPlayer = (currentPlayer == Color.WHITE)? Color.BLACK : Color.WHITE;
     }
     private void validateTargetPosition(Position source, Position target){
         if(!board.piece(source).possibleMove(target)){
